af48313d362290324bb9346c1b23954a3bc2e410
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-24 11:20:51 +0200

    Don't destruct the original array by slice method

--- mergesort.rb
+++ mergesort.rb
@@ -1,32 +1,30 @@
-require 'benchmark'
+# require 'benchmark'
 
 def merge(b, c)
-  i_b = 0
-  i_c = 0
-  b_size = b.size
-  c_size = c.size
   a = []
-  while i_b < b_size && i_c < c_size
-    if b.first < c.first
-      a << b.shift
-      i_b += 1
+  bi = 0
+  ci = 0
+  while b.size > bi && c.size > ci
+    if b[bi] < c[ci]
+      a << b[bi]
+      bi += 1
     else
-      a << c.shift
-      i_c += 1
+      a << c[ci]
+      ci += 1
     end
   end
-  if i_b == b_size
-    a = a + c
+  if bi == b.size
+    a = a + c[ci..-1]
   else
-    a = a + b
+    a = a + b[bi..-1]
   end
   a
 end
 
 def merge_sort(ary)
   if ary.size > 1
-    ary_b = ary.slice!(0, ary.size / 2)
-    ary_c = ary
+    ary_b = ary.slice(0, ary.size / 2)
+    ary_c = ary.slice((ary.size / 2)..-1)
     ary_b = merge_sort(ary_b)
     ary_c = merge_sort(ary_c)
     ary = merge(ary_b, ary_c)
@@ -34,12 +32,24 @@ def merge_sort(ary)
   ary
 end
 
-b = Benchmark.measure do
-  100000.times do
-    unsorted_ary = []
-    10.times { unsorted_ary << rand(100) }
-    p "#{unsorted_ary} should be sorted as #{unsorted_ary.sort}, actual:  #{merge_sort(unsorted_ary)}"
-    return "DAMI DAVIDO!!!" if unsorted_ary.sort != merge_sort(unsorted_ary)
+# b = Benchmark.measure do
+
+10.times do
+  unsorted_ary = []
+  10.times do
+    unsorted_ary << rand(100)
   end
+  sorted_by_sort = unsorted_ary.sort
+  print "#{unsorted_ary} should be sorted as #{sorted_by_sort},"
+  sorted_by_sortmerge = merge_sort(unsorted_ary)
+  puts " actual:  #{sorted_by_sortmerge}"
+  unless sorted_by_sort == sorted_by_sortmerge
+    raise StandardError, "you fucked up"
+  else
+    puts "It's ok"
+  end
+  puts sorted_by_sort == sorted_by_sortmerge
+  p unsorted_ary
 end
-puts b
+# end
+# puts b
